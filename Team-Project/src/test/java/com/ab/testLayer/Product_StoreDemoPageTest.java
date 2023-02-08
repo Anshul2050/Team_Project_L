d51d1867fc798afb40a042dbd11fb245252153e2
TinkuTalks <tinkubadgaiyan@gmail.com>
2023-02-08 18:36:56 +0530

    Product Demo commit
    This Commit is done by Anshul

---
+++ Team-Project/src/test/java/com/ab/testLayer/Product_StoreDemoPageTest.java
@@ -0,0 +1,12 @@
+package com.ab.testLayer;
+
+public class Product_StoreDemoPageTest {
+
+	
+	public void Test1() {
+		
+		System.out.println("Hello this page is controlled By Anshul");
+		
+	}
+	
+}
